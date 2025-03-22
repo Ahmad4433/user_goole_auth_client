ce912cade748026f567232fc5073a404ac7481a3
muhammad ahmad <110842437+Ahmad4433@users.noreply.github.com>
2025-03-22 12:30:09 +0500

    Update loginWithGoogle.js

--- src/utils/loginWithGoogle.js
+++ src/utils/loginWithGoogle.js
@@ -1,3 +1,3 @@
 export const loginWithGoogle = () => {
-  window.location.href = "http://localhost:8080/auth/google";
+  window.location.href = "https://user-google-auth-server.vercel.app/auth/google";
 };
