88e1e5ba904ff866eb8fa230ddea46c718933585
muhammad ahmad <110842437+Ahmad4433@users.noreply.github.com>
2025-03-22 12:28:55 +0500

    Update apis.js

--- src/utils/apis.js
+++ src/utils/apis.js
@@ -1,16 +1,17 @@
 const apis = () => {
   const local = "http://localhost:8080/";
+  const live = 'https://user-google-auth-server.vercel.app/'
 
   const list = {
-    loginUser: `${local}user/login`,
-    registerUser: `${local}user/register`,
-    getUserProfile: `${local}user/profile`,
-    getAccess: `${local}user/access`,
-    logout: `${local}user/logout`,
-    forgetPassword: `${local}user/password/forget`,
-    verifyOtp: `${local}user/otp/verify`,
-    getOtpExpTime: `${local}user/otp/exp`,
-    upadtePassword: `${local}user/password/update`,
+    loginUser: `${live}user/login`,
+    registerUser: `${live}user/register`,
+    getUserProfile: `${live}user/profile`,
+    getAccess: `${live}user/access`,
+    logout: `${live}user/logout`,
+    forgetPassword: `${live}user/password/forget`,
+    verifyOtp: `${live}user/otp/verify`,
+    getOtpExpTime: `${live}user/otp/exp`,
+    upadtePassword: `${live}user/password/update`,
   };
 
   return list;
